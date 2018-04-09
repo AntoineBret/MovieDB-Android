e50a8aaa3a789c6d0344c2cad455bb8aa06de268
Hadrien DE SOUSA <hadriendesousa@MBP-de-Hadrien.local>
2018-04-08 20:44:59 -0400

    Refactor reducer

--- app/src/main/java/com/hadeso/moviedb/ui/discovery/DiscoveryViewModel.kt
+++ app/src/main/java/com/hadeso/moviedb/ui/discovery/DiscoveryViewModel.kt
@@ -3,14 +3,15 @@ package com.hadeso.moviedb.ui.discovery
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.hadeso.moviedb.model.DiscoveryMovieModel
 import com.hadeso.moviedb.mvibase.MviViewModel
 import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 import io.reactivex.subjects.PublishSubject
 import javax.inject.Inject
 
 class DiscoveryViewModel @Inject constructor(
-        private val actionProcessorHolder: DiscoveryActionProcessorHolder,
-        private val reducer: DiscoveryReducer)
+        private val actionProcessorHolder: DiscoveryActionProcessorHolder)
     : ViewModel(), MviViewModel<DiscoveryIntent, DiscoveryViewState> {
 
     private val intentsSubject: PublishSubject<DiscoveryIntent> = PublishSubject.create()
@@ -32,7 +33,8 @@ class DiscoveryViewModel @Inject constructor(
         return intentsSubject
                 .map(this::actionFromIntent)
                 .compose(actionProcessorHolder.actionProcessor)
-                .compose(reducer.reducer)
+                .scan(DiscoveryViewState.idle(), reducer)
+                .distinctUntilChanged()
                 .replay(1)
                 .autoConnect(0)
     }
@@ -42,6 +44,34 @@ class DiscoveryViewModel @Inject constructor(
             is DiscoveryIntent.InitialIntent -> DiscoveryAction.LoadDiscoveryAction
             is DiscoveryIntent.MovieSelected -> DiscoveryAction.GoToMovieDetailAction(intent.discoveryViewItem)
 
+        }
+    }
+
+    companion object {
+        private val reducer = BiFunction { previousState: DiscoveryViewState, result: DiscoveryResult ->
+        when (result) {
+            is DiscoveryResult.LoadDiscoveryMoviesResult.Success ->
+        previousState.copy(isLoading = false,
+                error = null,
+                movies = result.movies.results.map { modelToView(it) })
+            is DiscoveryResult.LoadDiscoveryMoviesResult.Failure ->
+                previousState.copy(isLoading = false, error = result.error)
+            is DiscoveryResult.GotoMovieResult.Success ->
+                previousState.copy(isLoading = false,
+                        error = null,
+                        movies = result.movies.results.map { modelToView(it) })
+            is DiscoveryResult.GotoMovieResult.Failure ->
+                previousState.copy(isLoading = false, error = result.error)
+        }
+    }
+
+
+        private fun modelToView(discoveryMovieModel: DiscoveryMovieModel): DiscoveryViewItem {
+            return DiscoveryViewItem(discoveryMovieModel.id,
+                    discoveryMovieModel.title,
+                    discoveryMovieModel.posterPath,
+                    discoveryMovieModel.overview,
+                    discoveryMovieModel.voteAverage.toString())
         }
     }
 }
--- app/src/main/java/com/hadeso/moviedb/ui/discovery/DiscoveryViewState.kt
+++ app/src/main/java/com/hadeso/moviedb/ui/discovery/DiscoveryViewState.kt
@@ -6,4 +6,14 @@ data class DiscoveryViewState(
         val isLoading: Boolean,
         val movies: List<DiscoveryViewItem>,
         val error: Throwable?
-) : MviViewState
+) : MviViewState {
+    companion object {
+        fun idle(): DiscoveryViewState {
+            return DiscoveryViewState(
+                    isLoading = false,
+                    movies = emptyList(),
+                    error = null
+            )
+        }
+    }
+}
